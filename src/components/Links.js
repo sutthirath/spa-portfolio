b5f25f676c798ee492679bb8ea3e5d4f202f8c30
Sutthirath Sean Vilaysane <seanvilaysane@gmail.com>
2019-09-03 16:41:22 -0700

    added svg icons

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { Nav, NavItem, NavLink } from "reactstrap";
 
+import Email_Icon from "../images/email_icon.svg";
+import LinkedIn_Icon from "../images/linkedin_icon.svg";
+import Github_Icon from "../images/github_icon.svg";
+import Resume_Icon from "../images/resume_icon.svg";
+import Resume from "../images/resume.pdf";
+
 function Links() {
   return (
     <div style={styles.container}>
       <Nav style={styles.nav} horizontal>
         <NavItem style={styles.item}>
           <NavLink href="" style={styles.link}>
+            <img src={Email_Icon} alt="email_icon" />
             Email
           </NavLink>
         </NavItem>
@@ -15,16 +22,23 @@ function Links() {
             href="https://www.linkedin.com/in/seanvilaysane/"
             style={styles.link}
           >
+            <img src={LinkedIn_Icon} alt="linkedin_icon" />
             LinkedIn
           </NavLink>
         </NavItem>
         <NavItem style={styles.item}>
           <NavLink href="https://github.com/sutthirath" style={styles.link}>
+            <img src={Github_Icon} alt="github_icon" />
             Github
           </NavLink>
         </NavItem>
         <NavItem style={styles.item}>
-          <NavLink href="" style={styles.link}>
+          <NavLink
+            href={Resume}
+            download="SutthirathSeanVilaysane"
+            style={styles.link}
+          >
+            <img src={Resume_Icon} alt="resume_icon" />
             Resume
           </NavLink>
         </NavItem>
@@ -43,6 +57,7 @@ const styles = {
     padding: "30px"
   },
   link: {
+    display: "block",
     color: "white",
     textDecoration: "none"
   }
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,19 +1,25 @@
 import React from "react";
 
+import Email_Icon from "../images/email_icon.svg";
+import LinkedIn_Icon from "../images/linkedin_icon.svg";
+import Github_Icon from "../images/github_icon.svg";
+import Resume_Icon from "../images/resume_icon.svg";
+import Resume from "../images/resume.pdf";
+
 function SideBar() {
   return (
     <div style={styles.container}>
       <a href="" style={styles.link}>
-        Email
+        <img src={Email_Icon} alt="email_icon" />
       </a>
       <a href="https://www.linkedin.com/in/seanvilaysane/" style={styles.link}>
-        LinkedIn
+        <img src={LinkedIn_Icon} alt="linkedin_icon" />
       </a>
       <a href="https://github.com/sutthirath" style={styles.link}>
-        Github
+        <img src={Github_Icon} alt="github_icon" />
       </a>
-      <a href="" style={styles.link}>
-        Resume
+      <a href={Resume} download="SutthirathSeanVilaysane" style={styles.link}>
+        <img src={Resume_Icon} alt="resume_icon" />
       </a>
     </div>
   );
@@ -23,17 +29,19 @@ const styles = {
   container: {
     display: "flex",
     flexDirection: "column",
-    justifyContent: "center",
+    justifyContent: "space-around",
     alignItems: "center",
     backgroundColor: "rgba(255, 255, 255, 0.6)",
     height: "400px",
     width: "150px",
     borderTopRightRadius: "30px",
-    borderBottomRightRadius: "30px"
+    borderBottomRightRadius: "30px",
+    position: "fixed",
+    zIndex: "1"
   },
   link: {
     display: "block",
-    padding: "20px"
+    textDecoration: "none"
   }
 };
 
